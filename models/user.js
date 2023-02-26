2c3eaad53aac0da1765826df91fa1abc15b42b92
chrisIyer <hellohotbox420@gmail.com>
2023-02-26 11:07:23 -0500

    wow.delete

--- models/user.js
+++ models/user.js
@@ -5,19 +5,8 @@ const SALT_ROUNDS = 6
 
 const userSchema = new Schema({
   name: {type: String, required: true},
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    required: true
-  },
-  password: {
-    type: String,
-    trim: true,
-    minLength: 3,
-    required: true
-  }
+  email: { type: String, unique: true, trim: true, lowercase: true, required: true },
+  password: { type: String, trim: true, minLength: 3, required: true }
 }, {
   timestamps: true,
   toJSON: {
